4fb9d8ce05b3dfa7b6f91ecf96ee5f37bdd6a0b0
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-05-16 23:47:26 +0300

    reworked recipes for cooking pots
    replaced wooden shovel with stick

---
+++ mrpack/overrides/kubejs/server_scripts/farmers_delight_recipe.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(event => {
+    event.replaceInput({id: 'miners_delight:copper_pot'},
+        'minecraft:wooden_shovel',
+        'minecraft:stick'
+    )
+
+    event.replaceInput({id: 'farmersdelight:cooking_pot'},
+        'minecraft:wooden_shovel',
+        'minecraft:stick'
+    )
+})
