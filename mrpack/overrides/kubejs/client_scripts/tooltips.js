5f8af6aa79b671eef61a0ed840ead7051c1bae1a
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-05-17 20:53:24 +0300

    reworked immersive engineering bayonet recipe
    avoid usage of Steel Sword
    craft price not changed

--- mrpack/overrides/kubejs/client_scripts/tooltips.js
+++ mrpack/overrides/kubejs/client_scripts/tooltips.js
@@ -1,9 +1,3 @@
 ItemEvents.tooltip(event => {
-  event.add('tconstruct:pickadze', Text.translate('tooltip.modpack.aka_shovel').gray().italic());
+    event.add('tconstruct:pickadze', Text.translate('tooltip.modpack.aka_shovel').gray().italic());
 });
-
-// ItemEvents.tooltip(event => {
-//   event.addAdvanced('tconstruct:pickadze', (item, advanced, text) => {
-//     text.splice(1, 0, Text.translate('tooltip.modpack.aka_shovel').gray().italic());
-//   });
-// });
