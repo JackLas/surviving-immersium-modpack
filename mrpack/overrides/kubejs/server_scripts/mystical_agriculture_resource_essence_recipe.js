94dd7dfd8b291867a4fd899c200e793d7de24948
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-07-07 02:29:07 +0300

    rebalance mystical agriculture essence recipes
    update for dirt, stone, deepslate, nether, end, ice, amethyst essences
    add essence recipes for new quark stones and corundum
    add essence recipes for tinker construct's slime crystals
    refactor kubejs code

--- mrpack/overrides/kubejs/server_scripts/mystical_agriculture_resource_essence_recipe.js
+++ mrpack/overrides/kubejs/server_scripts/mystical_agriculture_resource_essence_recipe.js
@@ -1,4 +1,19 @@
 ServerEvents.recipes(event => {  
+    function addCommonRecipe(recipeCoreItemId, recipeSidesItemId, outputItemId, outputCount, keepCore) {
+        var recipe = event.shaped(Item.of(outputItemId, outputCount), [
+            "SSS",
+            "SCS", 
+            "SSS"
+        ], {
+            C: recipeCoreItemId,
+            S: recipeSidesItemId
+        })
+
+        if (keepCore) {
+            recipe.keepIngredient(recipeCoreItemId)
+        }
+    }
+
     event.remove({id: "mysticalagriculture:essence/minecraft/chorus_fruit"})
     event.remove({id: "mysticalagriculture:essence/appliedenergistics2/engineering_press"})
     event.remove({id: "mysticalagriculture:essence/appliedenergistics2/logic_press"})
@@ -27,34 +42,9 @@ ServerEvents.recipes(event => {
     event.remove({id: "mysticalagriculture:essence/minecraft/cactus"})
     event.remove({id: "mysticalagriculture:essence/minecraft/cocoa_beans"})
     event.remove({id: "mysticalagriculture:essence/minecraft/nether_bricks"})
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/obsidian"})
-    event.shaped(Item.of("minecraft:obsidian", 4), [
-        "EEE",
-        "EEE", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:obsidian_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/cobblestone"})
-    event.shaped(Item.of("minecraft:cobblestone", 16), [
-        "EEE",
-        "E E", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:stone_essence"
-    })
-
     event.remove({id: "mysticalagriculture:essence/minecraft/stone"})
-    event.shaped(Item.of("minecraft:stone", 8), [
-        "EEE",
-        "ECE", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:stone_essence",
-        C: "mysticalagriculture:coal_essence"
-    })
+    event.remove({id: "mysticalagriculture:essence/minecraft/deepslate"})
+    event.remove({id: "mysticalagriculture:essence/minecraft/packed_ice"})
 
     event.shaped(Item.of("minecraft:clay_ball", 4), [
         " M ",
@@ -65,65 +55,139 @@ ServerEvents.recipes(event => {
         M: "kubejs:mud_ball"
     })
 
-    event.remove({id: "mysticalagriculture:essence/minecraft/dirt"})
-    event.shaped(Item.of("minecraft:dirt", 16), [
-        "EEE",
-        "E E", 
-        "EEE"
+    event.remove({id: "mysticalagriculture:essence/minecraft/nether_wart"})
+    event.shaped(Item.of("minecraft:nether_wart", 8), [
+        "NEN",
+        "EWE", 
+        "NEN"
     ], {
-        E: "mysticalagriculture:dirt_essence"
-    })
+        N: "mysticalagriculture:nature_essence",
+        E: "mysticalagriculture:nether_essence",
+        W: "minecraft:nether_wart"
+    }).keepIngredient("minecraft:nether_wart")
+
+    
+    event.remove({id: "mysticalagriculture:essence/minecraft/coal"})
+    addCommonRecipe(null, "mysticalagriculture:coal_essence", "minecraft:coal", 8)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/netherite_ingot"})
+    addCommonRecipe("mysticalagriculture:gold_essence", "mysticalagriculture:netherite_essence", "minecraft:netherite_ingot", 1)
+
+    addCommonRecipe("quark:permafrost", "mysticalagriculture:ice_essence", "quark:permafrost", 16)
+
+    // amethyst
+    event.remove({id: "mysticalagriculture:essence/minecraft/amethyst"})
+    addCommonRecipe(null, "mysticalagriculture:amethyst_essence", "minecraft:amethyst_shard", 8)
+    addCommonRecipe("tconstruct:earth_slime_crystal", "mysticalagriculture:amethyst_essence", "tconstruct:earth_slime_crystal", 4, true)
+    addCommonRecipe("tconstruct:sky_slime_crystal", "mysticalagriculture:amethyst_essence", "tconstruct:sky_slime_crystal", 4, true)
+    addCommonRecipe("tconstruct:ender_slime_crystal", "mysticalagriculture:amethyst_essence", "tconstruct:ender_slime_crystal", 4, true)
+    addCommonRecipe("tconstruct:ichor_slime_crystal", "mysticalagriculture:amethyst_essence", "tconstruct:ichor_slime_crystal", 4, true)
+    addCommonRecipe("quark:red_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:red_corundum_cluster", 8, true)
+    addCommonRecipe("quark:orange_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:orange_corundum_cluster", 8, true)
+    addCommonRecipe("quark:yellow_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:yellow_corundum_cluster", 8, true)
+    addCommonRecipe("quark:green_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:green_corundum_cluster", 8, true)
+    addCommonRecipe("quark:blue_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:blue_corundum_cluster", 8, true)
+    addCommonRecipe("quark:indigo_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:indigo_corundum_cluster", 8, true)
+    addCommonRecipe("quark:violet_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:violet_corundum_cluster", 8, true)
+    addCommonRecipe("quark:white_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:white_corundum_cluster", 8, true)
+    addCommonRecipe("quark:black_corundum_cluster", "mysticalagriculture:amethyst_essence", "quark:black_corundum_cluster", 8, true)
+    function addCorundumBlockRecipe(colorStr) {
+        const clusterId = "quark:" + colorStr + "_corundum_cluster"
+        const blockId = "quark:" + colorStr + "_corundum"
+        event.shaped(Item.of(blockId, 1), [
+            "CC",
+            "CC"
+        ], {
+            C: clusterId
+        })
+    }
+    addCorundumBlockRecipe("red")
+    addCorundumBlockRecipe("orange")
+    addCorundumBlockRecipe("yellow")
+    addCorundumBlockRecipe("green")
+    addCorundumBlockRecipe("blue")
+    addCorundumBlockRecipe("indigo")
+    addCorundumBlockRecipe("violet")
+    addCorundumBlockRecipe("white")
+    addCorundumBlockRecipe("black")
+
+    // obsidian
+    event.remove({id: "mysticalagriculture:essence/minecraft/obsidian"})
+    addCommonRecipe(null, "mysticalagriculture:obsidian_essence", "minecraft:obsidian", 4)
+
+    // deepslate
+    event.remove({id: "mysticalagriculture:essence/minecraft/cobbled_deepslate"})
+    addCommonRecipe(null, "mysticalagriculture:deepslate_essence", "minecraft:cobbled_deepslate", 16)
+
+    // dirt
+    event.remove({id: "mysticalagriculture:essence/minecraft/dirt"})
+    addCommonRecipe(null, "mysticalagriculture:dirt_essence", "minecraft:dirt", 16)
 
     event.remove({id: "mysticalagriculture:essence/minecraft/grass_block"})
-    event.shaped(Item.of("minecraft:grass_block", 8), [
-        "ENE",
-        "ENE", 
-        "ENE"
-    ], {
-        E: "mysticalagriculture:dirt_essence",
-        N: "mysticalagriculture:nature_essence"
-    })
+    addCommonRecipe("minecraft:grass_block", "mysticalagriculture:dirt_essence", "minecraft:grass_block", 16, true)
 
     event.remove({id: "mysticalagriculture:essence/minecraft/podzol"})
-    event.shaped(Item.of("minecraft:podzol", 8), [
-        "EEE",
-        "NNN", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:dirt_essence",
-        N: "mysticalagriculture:nature_essence"
-    })
+    addCommonRecipe("minecraft:podzol", "mysticalagriculture:dirt_essence", "minecraft:podzol", 16, true)
 
     event.remove({id: "mysticalagriculture:essence/minecraft/mycelium"})
-    event.shaped(Item.of("minecraft:mycelium", 8), [
-        "NEE",
-        "ENE", 
-        "EEN"
-    ], {
-        E: "mysticalagriculture:dirt_essence",
-        N: "mysticalagriculture:nature_essence"
-    })
+    addCommonRecipe("minecraft:mycelium", "mysticalagriculture:dirt_essence", "minecraft:mycelium", 16, true)
 
-    function addWoodRecipes(saplingId, woodId) {
-        event.shaped(Item.of(woodId, 8), [
-            "EEE",
-            "ESE", 
-            "EEE"
-        ], {
-            E: "mysticalagriculture:wood_essence",
-            S: saplingId
-        }).keepIngredient(saplingId)
+    // stone
+    event.remove({id: "mysticalagriculture:essence/minecraft/cobblestone"})
+    addCommonRecipe(null, "mysticalagriculture:stone_essence", "minecraft:cobblestone", 16)
 
-        event.shaped(Item.of(saplingId, 8), [
-            "EEE",
-            "ESE", 
-            "EEE"
-        ], {
-            E: "mysticalagriculture:nature_essence",
-            S: saplingId
-        }).keepIngredient(saplingId)
-    }
+    event.remove({id: "mysticalagriculture:essence/minecraft/calcite"})
+    addCommonRecipe("minecraft:calcite", "mysticalagriculture:stone_essence", "minecraft:calcite", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/granite"})
+    addCommonRecipe("minecraft:granite", "mysticalagriculture:stone_essence", "minecraft:granite", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/diorite"})
+    addCommonRecipe("minecraft:diorite", "mysticalagriculture:stone_essence", "minecraft:diorite", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/andesite"})
+    addCommonRecipe("minecraft:andesite", "mysticalagriculture:stone_essence", "minecraft:andesite", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/tuff"})
+    addCommonRecipe("minecraft:tuff", "mysticalagriculture:stone_essence", "minecraft:tuff", 16, true)
+
+    addCommonRecipe("quark:limestone", "mysticalagriculture:stone_essence", "quark:limestone", 16, true)
+    addCommonRecipe("quark:jasper", "mysticalagriculture:stone_essence", "quark:jasper", 16, true)
+    addCommonRecipe("quark:shale", "mysticalagriculture:stone_essence", "quark:shale", 16, true)
+
+    // end 
+    event.remove({id: "mysticalagriculture:essence/minecraft/end_stone"})
+    addCommonRecipe(null, "mysticalagriculture:end_essence", "minecraft:end_stone", 16)
+    addCommonRecipe("quark:myalite", "mysticalagriculture:end_essence", "quark:myalite", 16, true)
+
+    // nether
+    event.remove({id: "mysticalagriculture:essence/minecraft/netherrack"})
+    addCommonRecipe(null, "mysticalagriculture:nether_essence", "minecraft:netherrack", 16)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/crimson_nylium"})
+    addCommonRecipe("minecraft:crimson_nylium", "mysticalagriculture:nether_essence", "minecraft:crimson_nylium", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/warped_nylium"})
+    addCommonRecipe("minecraft:warped_nylium", "mysticalagriculture:nether_essence", "minecraft:warped_nylium", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/blackstone"})
+    addCommonRecipe("minecraft:blackstone", "mysticalagriculture:nether_essence", "minecraft:blackstone", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/basalt"})
+    addCommonRecipe("minecraft:basalt", "mysticalagriculture:nether_essence", "minecraft:basalt", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/soul_soil"})
+    addCommonRecipe("minecraft:soul_soil", "mysticalagriculture:nether_essence", "minecraft:soul_soil", 16, true)
+
+    event.remove({id: "mysticalagriculture:essence/minecraft/soul_sand"})
+    addCommonRecipe("minecraft:soul_sand", "mysticalagriculture:nether_essence", "minecraft:soul_sand", 16, true)
+
+    // wood
     event.remove({input: "mysticalagriculture:wood_essence"})
+    function addWoodRecipes(saplingId, woodId) {
+        addCommonRecipe(saplingId, "mysticalagriculture:wood_essence", woodId, 8, true)
+        addCommonRecipe(saplingId, "mysticalagriculture:nature_essence", saplingId, 8, true)
+    }
     addWoodRecipes("minecraft:oak_sapling", "minecraft:oak_log")
     addWoodRecipes("minecraft:spruce_sapling", "minecraft:spruce_log")
     addWoodRecipes("minecraft:birch_sapling", "minecraft:birch_log")
@@ -146,130 +210,4 @@ ServerEvents.recipes(event => {
     addWoodRecipes("quark:orange_blossom_sapling", "quark:blossom_log")
     addWoodRecipes("quark:yellow_blossom_sapling", "quark:blossom_log")
     addWoodRecipes("quark:red_blossom_sapling", "quark:blossom_log")
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/cobbled_deepslate"})
-    event.shaped(Item.of("minecraft:cobbled_deepslate", 16), [
-        "EEE",
-        "E E", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:deepslate_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/deepslate"})
-    event.shaped(Item.of("minecraft:deepslate", 8), [
-        "EEE",
-        "ECE", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:deepslate_essence",
-        C: "mysticalagriculture:coal_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/nether_wart"})
-    event.shaped(Item.of("minecraft:nether_wart", 8), [
-        "NEN",
-        "EWE", 
-        "NEN"
-    ], {
-        N: "mysticalagriculture:nature_essence",
-        E: "mysticalagriculture:nether_essence",
-        W: "minecraft:nether_wart"
-    }).keepIngredient("minecraft:nether_wart")
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/crimson_nylium"})
-    event.shaped(Item.of("minecraft:crimson_nylium", 8), [
-        "ENE",
-        "ENE", 
-        "ENE"
-    ], {
-        E: "mysticalagriculture:nether_essence",
-        N: "mysticalagriculture:nature_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/warped_nylium"})
-    event.shaped(Item.of("minecraft:warped_nylium", 8), [
-        "EEE",
-        "NNN", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:nether_essence",
-        N: "mysticalagriculture:nature_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/blackstone"})
-    event.shaped(Item.of("minecraft:blackstone", 16), [
-        "NNN",
-        "NSN", 
-        "NNN"
-    ], {
-        S: "mysticalagriculture:stone_essence",
-        N: "mysticalagriculture:nether_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/basalt"})
-    event.shaped(Item.of("minecraft:basalt", 16), [
-        "NNN",
-        "NIN", 
-        "NNN"
-    ], {
-        I: "mysticalagriculture:ice_essence",
-        N: "mysticalagriculture:nether_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/netherrack"})
-    event.shaped(Item.of("minecraft:netherrack", 16), [
-        "NNN",
-        "N N", 
-        "NNN"
-    ], {
-        N: "mysticalagriculture:nether_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/coal"})
-    event.shaped(Item.of("minecraft:coal", 8), [
-        "CCC",
-        "C C", 
-        "CCC"
-    ], {
-        C: "mysticalagriculture:coal_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/amethyst"})
-    event.shaped(Item.of("minecraft:amethyst_shard", 8), [
-        "AAA",
-        "A A", 
-        "AAA"
-    ], {
-        A: "mysticalagriculture:amethyst_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/calcite"})
-    event.shaped(Item.of("minecraft:calcite", 16), [
-        "SSS",
-        "SAS", 
-        "SSS"
-    ], {
-        S: "mysticalagriculture:stone_essence",
-        A: "mysticalagriculture:amethyst_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/end_stone"})
-    event.shaped(Item.of("minecraft:end_stone", 16), [
-        "EEE",
-        "E E", 
-        "EEE"
-    ], {
-        E: "mysticalagriculture:end_essence"
-    })
-
-    event.remove({id: "mysticalagriculture:essence/minecraft/netherite_ingot"})
-    event.shaped(Item.of("minecraft:netherite_ingot", 1), [
-        "NNN",
-        "NGN", 
-        "NNN"
-    ], {
-        N: "mysticalagriculture:netherite_essence",
-        G: "mysticalagriculture:gold_essence"
-    })
 })
