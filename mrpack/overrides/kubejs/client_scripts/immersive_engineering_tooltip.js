8dc52b522c1b7f523788cdc29a4f35997881247d
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-07-09 19:55:24 +0300

    add tooltips
    - Crimson Boat
    - Crimson Boat with Chest
    - Warped Boat
    - Warped Boat with Chest
    - Faraday Helmet
    - Faraday Chestplate
    - Faraday Leggings
    - Faraday Boots
    - Painting

---
+++ mrpack/overrides/kubejs/client_scripts/immersive_engineering_tooltip.js
@@ -0,0 +1,24 @@
+ItemEvents.tooltip(event => {
+    function addSubtitleTextForArmor(itemId, text1, text2) {
+        event.addAdvanced(itemId, (item, advanced, tooltip) => {
+            tooltip.add(1, text1)
+            tooltip.add(2, text2)
+        });
+    }
+
+    addSubtitleTextForArmor("immersiveengineering:armor_faraday_helmet", 
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection1"),
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection2"))
+
+    addSubtitleTextForArmor("immersiveengineering:armor_faraday_chestplate", 
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection1"),
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection2"))
+
+    addSubtitleTextForArmor("immersiveengineering:armor_faraday_leggings", 
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection1"),
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection2"))
+
+    addSubtitleTextForArmor("immersiveengineering:armor_faraday_boots", 
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection1"),
+                            descriptionText("tooltip.kubejs.immersive_engineering.faradey_protection2"))
+});
--- mrpack/overrides/kubejs/client_scripts/tooltips.js
+++ mrpack/overrides/kubejs/client_scripts/tooltips.js
@@ -1,3 +1,7 @@
 ItemEvents.tooltip(event => {
     event.add("tconstruct:pickadze", Text.translate("tooltip.kubejs.aka_shovel").gray().italic());
+
+    event.addAdvanced("minecraft:painting", (item, advanced, tooltip) => {
+        tooltip.add(1, descriptionText("tooltip.kubejs.painting"))
+    });
 });
