b26420073927104dc20abddfa87476ee6c5b615c
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-07-20 00:02:26 +0300

    add bear pelt items
    - add bear pelt that drops from Grizzly Bear
    - add polar bear pelt that drops from Polar Bear
    - use any bear pelt in backpack craft

---
+++ mrpack/overrides/kubejs/server_scripts/bear_pelt_drop.js
@@ -0,0 +1,10 @@
+LootJS.modifiers((event) => {
+    function addLoot(mobId, itemId) {
+        event.addEntityLootModifier(mobId)
+             .randomChanceWithEnchantment("minecraft:looting", [1.0, 1.0, 1.0, 1.0]) 
+             .addLoot(itemId) 
+    }
+
+    addLoot("alexsmobs:grizzly_bear", "kubejs:bear_pelt") 
+    addLoot("minecraft:polar_bear", "kubejs:polar_bear_pelt") 
+});
---
+++ mrpack/overrides/kubejs/startup_scripts/item_bear_pelt.js
@@ -0,0 +1,7 @@
+StartupEvents.registry("item", event => {
+    event.create("bear_pelt")
+         .maxStackSize(64)
+         .tag("forge:pelt/bear")
+         .tag("forge:leather")
+         .tag("c:leathers")
+})
