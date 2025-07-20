047d04886033e25f37408139ae54735e0f920ebd
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-07-20 18:45:11 +0300

    refactor kubejs

--- mrpack/overrides/kubejs/client_scripts/tooltips/vanilla.js
+++ mrpack/overrides/kubejs/client_scripts/tooltips/vanilla.js
@@ -1,6 +1,4 @@
 ItemEvents.tooltip(event => {
-    event.add("tconstruct:pickadze", Text.translate("tooltip.kubejs.aka_shovel").gray().italic())
-
     event.addAdvanced("minecraft:painting", (item, advanced, tooltip) => {
         tooltip.add(1, descriptionText("tooltip.kubejs.painting"))
     })
