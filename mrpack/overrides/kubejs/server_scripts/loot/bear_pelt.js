e7d2380df178aedee88fd000e94c1b9fa3237cbb
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-09-27 22:29:06 +0300

    increase bear felt drop with looting

--- mrpack/overrides/kubejs/server_scripts/loot/bear_pelt.js
+++ mrpack/overrides/kubejs/server_scripts/loot/bear_pelt.js
@@ -1,7 +1,7 @@
 LootJS.modifiers((event) => {
     function addLoot(mobId, itemId) {
         event.addEntityLootModifier(mobId)
-             .randomChanceWithEnchantment("minecraft:looting", [1.0, 1.0, 1.0, 1.0]) 
+             .randomChanceWithEnchantment("minecraft:looting", [1.0, 1.3, 1.6, 1.9]) 
              .addLoot(itemId) 
     }
 
