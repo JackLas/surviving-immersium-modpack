2400f4fdcd5bbf4c8e1808b27af57cc92fd94160
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-09-27 22:07:32 +0300

    remove enchanter

---
+++ mrpack/overrides/kubejs/server_scripts/recipes/mystical_agriculture_enchanter.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    // don't like it
+    // also, broken recipes in EMI, doesn't show items count
+    
+    event.remove({type: "mysticalagriculture:enchanter"})
+})
+
