8b560200e513d9bb8977a00912519f373da32c10
Yevhenii Kryvyi <jackgm555@gmail.com>
2025-09-27 22:08:33 +0300

    revise sophisticated upgrades

---
+++ mrpack/overrides/kubejs/server_scripts/fixes/sophisitcated_upgrades_recipes_fix.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.remove({id: "sophisticatedstorage:storage_stack_upgrade_tier_1_plus_from_backpack_stack_upgrade_starter_tier"})
+    event.remove({id: "sophisticatedstorage:storage_stack_upgrade_tier_2_from_backpack_stack_upgrade_tier_1"})
+    event.remove({id: "sophisticatedstorage:storage_stack_upgrade_tier_3_from_backpack_stack_upgrade_tier_2"})
+    event.remove({id: "sophisticatedstorage:storage_stack_upgrade_tier_4_from_backpack_stack_upgrade_tier_3"})
+    event.remove({id: "sophisticatedstorage:storage_stack_upgrade_tier_5_from_backpack_stack_upgrade_tier_4"})
+    event.remove({id: "sophisticatedstorage:backpack_alchemy_upgrade_from_storage_alchemy_upgrade"})
+    event.remove({id: "sophisticatedstorage:backpack_advanced_alchemy_upgrade_from_storage_advanced_alchemy_upgrade"})
+})
